423a01c5ece1631521a071901f141f51efb7f067
boundino <boundino@gmail.com>
2025-07-02 13:25:38 +0200

    add pO

--- Run2025OO536/MinBias/python/MinBias_Pythia_Angantyr_pO_9617GeV.py
+++ Run2025OO536/MinBias/python/MinBias_Pythia_Angantyr_pO_9617GeV.py
@@ -5,14 +5,15 @@ generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
     pythiaPylistVerbosity = cms.untracked.int32(1),
     filterEfficiency = cms.untracked.double(1.0),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
-    comEnergy = cms.double(5362.),
+    comEnergy = cms.double(9617.),
     HeavyIonInitialState = cms.PSet( ),
     PythiaParameters = cms.PSet(
         processParameters = cms.vstring(
             'Beams:idA = 1000080160',
-            'Beams:idB = 1000080160',
-            'Beams:frameType = 1',
-            'Beams:eCM = 5362.',
+            'Beams:idB = 2212',
+            'Beams:frameType = 2',
+            'Beams:eA = 3400',
+            'Beams:eB = 6800',
             
             # MinBias events
             'SoftQCD:inelastic = on',
@@ -21,11 +22,21 @@ generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
             'HeavyIon:mode = 1',
             # HeavyIon fitting of SubCollisionModel to cross sections
             'HeavyIon:SigFitNGen = 0',
-            'HeavyIon:SigFitDefPar = 2.18,17.55,0.32',
+            'HeavyIon:SigFitDefPar = 2.23,17.27,0.30',
             
             # Harmonic Oscillator Shell model (light ion geometry)
             'Angantyr:NucleusModelA = 3',
-            'Angantyr:NucleusModelB = 3',
+
+            #Forward tune (https://arxiv.org/pdf/2309.08604)
+            'BeamRemnants:dampPopcorn=0',
+            'BeamRemnants:hardRemnantBaryon=on',
+            'BeamRemnants:aRemnantBaryon=0.36',
+            'BeamRemnants:bRemnantBaryon=1.69',
+            'BeamRemnants:primordialKTsoft=0.58',
+            'BeamRemnants:primordialKThard=1.8',
+            'BeamRemnants:halfScaleForKT=10',
+            'BeamRemnants:halfMassForKT=1',
+            'BeamRemnants:primordialKTremnant=0.58',
         ),
         parameterSets = cms.vstring('processParameters')
     )
