719a727cd0ce527abd07da1e2580f3b791c5a19b
boundino <boundino@gmail.com>
2026-07-07 19:35:58 +0200

    add softQCD upc D frag

--- Run3PbPb536/UPCHF/python/Pythia8_DzeroToKPi_nonprompt_GNucleus-PhotonBeamA_SoftQCD_5362GeV.py
+++ Run3PbPb536/UPCHF/python/Pythia8_DzeroToKPi_nonprompt_GNucleus-PhotonBeamA_SoftQCD_5362GeV.py
@@ -12,31 +12,16 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
     comEnergy = cms.double(5362.),
     PhotonFlux = PhotonFlux_PbPb,
     ExternalDecays = cms.PSet(
-        EvtGen130 = cms.untracked.PSet(
-            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
-            operates_on_particles = cms.vint32(),
-            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
-            list_forced_decays = cms.vstring('myD0', 'myanti-D0'),
-            user_decay_embedded = cms.vstring(
-                """
-                Alias myD0      D0
-                Alias myanti-D0 anti-D0
-                ChargeConj myanti-D0 myD0
-                
-                Decay myD0
-                0.87701  K-      pi+  PHSP;
-                0.09069  K+      K-   PHSP;
-                0.03231  pi+     pi-  PHSP;
-                Enddecay
-                CDecay myanti-D0
-                
-                End
-                """
-            ),
-            convertPythiaCodes = cms.untracked.bool(False),
-        ),
-    parameterSets = cms.vstring('EvtGen130')
-    ),
+                             EvtGen130 = cms.untracked.PSet(
+                                     decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+                                     operates_on_particles = cms.vint32(),
+                                     particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+                                     user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/D0_Kpi.dec'),
+                                     list_forced_decays = cms.vstring('myD0', 'myanti-D0'),
+                                     convertPythiaCodes = cms.untracked.bool(False),
+                             ),
+                             parameterSets = cms.vstring('EvtGen130')
+                     ),
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         processParameters = cms.vstring('SoftQCD:all = on',
@@ -60,8 +45,18 @@ generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
 "_generator=cms.EDFilter fromGeneratorInterface.Core.ExternalGeneratorFilterimportExternalGeneratorFilter generator=ExternalGeneratorFilter(_generator)"
 
 
-D0filter = cms.EDFilter("PythiaFilter",
-                        ParticleID = cms.untracked.int32(421)
+partonfilter = cms.EDFilter("PythiaFilter",
+                                    ParticleID = cms.untracked.int32(5) #non-prompt
+                                    )
+
+D0Daufilter = cms.EDFilter("PythiaMomDauFilter",
+    ParticleID = cms.untracked.int32(421),
+    MomMinPt = cms.untracked.double(0.),
+        MomMinEta = cms.untracked.double(-10),
+        MomMaxEta = cms.untracked.double(10),
+    DaughterIDs = cms.untracked.vint32(-321, 211),
+    NumberDaughters = cms.untracked.int32(2),
+    NumberDescendants = cms.untracked.int32(0),
 )
 
-ProductionFilterSequence = cms.Sequence(generator*D0filter)
+ProductionFilterSequence = cms.Sequence(generator*partonfilter*D0Daufilter)
---
+++ Run3PbPb536/UPCHF/python/Pythia8_GNucleus-PhotonBeamA_SoftQCD_5362GeV.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8PhotonFluxSettings_cfi import PhotonFlux_PbPb
+
+
+generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(5362.),
+    PhotonFlux = PhotonFlux_PbPb,
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        processParameters = cms.vstring('SoftQCD:all = on',
+                                        'PhaseSpace:pTHatMin = 0.',
+                                        'PhotonParton:all = on',
+                                        'MultipartonInteractions:pT0Ref = 3.0',
+                                        'PDF:beamA2gamma = on',# have the photon coming from beam A
+                                        'PDF:proton2gammaSet = 0',
+                                        'PDF:pSetB = LHAPDF6:EPPS21nlo_CT18Anlo_Pb208', # add nuclear modifications to beam B
+                                        'PDF:gammaFluxApprox2bMin = 13.272',
+                                        'PDF:beam2gammaApprox = 2',
+                                        'Photon:sampleQ2 = off'
+                                    ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'processParameters')
+    )
+)
